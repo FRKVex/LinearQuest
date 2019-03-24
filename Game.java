520cab9add5d90b3fa16cfe97a8b255cec2405c2
FRKVex <vhen.pataueg@gmail.com>
2019-03-24 23:30:08 +0800

    added map scrolling

--- Game.java
+++ Game.java
@@ -1,7 +1,9 @@
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 public class Game extends Canvas implements Runnable{
 
@@ -10,15 +12,22 @@ public class Game extends Canvas implements Runnable{
 	private boolean isRunning = false;
 	private Thread thread;
 	private Handler handler;
+	private Camera camera;
+	
+	private BufferedImage level = null;
 	
 	public Game(){
 		new Frame(1000,563,"Linear Quest", this);
 		start();
 		
 		handler = new Handler();
+		camera = new Camera(0, 0);
 		this.addKeyListener(new Control(handler));
 		
-		handler.addObject(new Player(100, 100, ID.Player, handler));
+		BufferedImageLoader loader = new BufferedImageLoader();
+		level = loader.loadImage("/Map1.png");
+
+		loadLevel(level);
 	}
 	
 	public void start() {
@@ -64,6 +73,13 @@ public class Game extends Canvas implements Runnable{
 	}
 	
 	public void tick() {
+		
+		for(int i = 0; i < handler.object.size(); i++){
+			if(handler.object.get(i).getId() == ID.Player){
+				camera.tick(handler.object.get(i));
+			}
+		}	
+		
 		handler.tick();
 	}
 	
@@ -75,16 +91,40 @@ public class Game extends Canvas implements Runnable{
 		}
 		
 		Graphics g = bs.getDrawGraphics();
+		Graphics2D g2d = (Graphics2D) g;
 		//
 		
 		g.setColor(Color.BLUE);
 		g.fillRect(0, 0, 1000, 563);
 		
+		g2d .translate(-camera.getX(), -camera.getY());
+
 		handler.render(g);
-		
+
+		g2d .translate(camera.getX(), camera.getY());
 		//
 		g.dispose();
 		bs.show();
+	}
+	
+	private void loadLevel(BufferedImage image){
+		int w = image.getWidth();
+		int h = image.getHeight();
+
+		for(int xx = 0; xx < w; xx++){
+			for(int yy = 0;yy < h; yy++){
+				int pixel = image.getRGB(xx, yy);
+				int red = (pixel >> 16) & 0xff;
+				int green = (pixel >> 8) & 0xff;
+				int blue = (pixel) & 0xff;
+
+				if(red == 255)
+					handler.addObject(new Block(xx*32, yy*32, ID.Block));
+
+				if(blue == 255)
+					handler.addObject(new Player(xx*32, yy*32, ID.Player, handler));
+			}
+		}
 	}
 
 	public static void main(String args[]){
